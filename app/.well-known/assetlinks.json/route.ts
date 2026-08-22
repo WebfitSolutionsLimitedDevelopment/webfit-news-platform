63bf799f68e1f93285f32952e7d59b2f3832c56d
WebfitSolutionsLimitedDevelopment <webfittsolutions@gmail.com>
2026-08-23 09:37:12 +1200

    updated value of fingerPrint

--- app/.well-known/assetlinks.json/route.ts
+++ app/.well-known/assetlinks.json/route.ts
@@ -9,7 +9,7 @@ export async function GET() {
       target: {
         namespace: 'android_app',
         package_name: 'com.webfitnews.app',
-        sha256_cert_fingerprints: ['PASTE_YOUR_FINGERPRINT_HERE'],
+        sha256_cert_fingerprints: ['96:DC:84:6D:1A:2D:E0:B7:54:E9:80:66:D7:3E:45:70:16:80:20:7A:C1:05:B7:CD:7F:F3:4A:C8:A0:EF:42:94'],
       },
     },
   ]);
