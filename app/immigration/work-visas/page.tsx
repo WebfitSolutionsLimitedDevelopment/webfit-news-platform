df822ac6caeaede9871b9325c2e8d5873181b865
WebfitSolutionsLimitedDevelopment <srikantnz022024@gmail.com>
2026-09-09 11:15:11 +1200

    Normalize work visa canonical host

--- app/immigration/work-visas/page.tsx
+++ app/immigration/work-visas/page.tsx
@@ -3,7 +3,7 @@ import { VisaCategoryLanding } from '@/components/immigration/VisaCategoryLandin
 import { getImmigrationCategoryPage } from '@/lib/immigration-category-pages';
 
 const config = getImmigrationCategoryPage('work-visas')!;
-const url = 'https://www.webfitnews.com/immigration/work-visas';
+const url = 'https://webfitnews.com/immigration/work-visas';
 
 export const metadata: Metadata = {
   title: config.metaTitle,
