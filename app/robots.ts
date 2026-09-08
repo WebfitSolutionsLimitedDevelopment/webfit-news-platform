ff5b384afa41457a83e29cba162d5852188c0876
WebfitSolutionsLimitedDevelopment <srikantnz022024@gmail.com>
2026-09-09 08:05:39 +1200

    Expose immigration sitemap directly to crawlers

--- app/robots.ts
+++ app/robots.ts
@@ -1,10 +1,14 @@
 import type { MetadataRoute } from 'next';
 
 const SITE_URL = 'https://webfitnews.com';
+const IMMIGRATION_SITE_URL = 'https://www.webfitnews.com';
 
 export default function robots():MetadataRoute.Robots{
   return {
     rules:{userAgent:'*',allow:'/',disallow:['/admin/','/api/']},
-    sitemap:`${SITE_URL}/sitemap.xml`
+    sitemap:[
+      `${SITE_URL}/sitemap.xml`,
+      `${IMMIGRATION_SITE_URL}/immigration/sitemap.xml`
+    ]
   };
 }
